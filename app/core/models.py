1079b17b953c21bd3b7e0c14eefe416531e7f954
jsm <jsmoretti.dev@outlook.com>
2025-08-18 19:50:33 -0300

    feat: create waitress table and alambic setup

--- app/core/models.py
+++ app/core/models.py
@@ -1,12 +1,8 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import Column, DateTime, Integer, String
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, relationship
 
-# from typing import TYPE_CHECKING
-# TODO -> check if this errors
-# if TYPE_CHECKING:
-#     from users.model import User
 
 class Base(DeclarativeBase):
     pass
@@ -16,9 +12,11 @@ class UserModel(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
+
     created_at = Column(DateTime, default=datetime.now(timezone.utc))
     updated_at = Column(
         DateTime,
@@ -26,3 +24,30 @@ class UserModel(Base):
         onupdate=datetime.now(timezone.utc),
     )
     deleted_at = Column(DateTime, nullable=True)
+
+    waitress_profile = relationship(
+        "WaitressModel",
+        back_populates="user",
+        uselist=False,
+    )
+
+
+class WaitressModel(Base):
+    __tablename__ = "waitress"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"), unique=True, nullable=False)
+
+    user = relationship("UserModel", back_populates="waitress_profile")
+
+
+# class CookModel(Base):
+#     __tablename__ = "cook"
+
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+
+
+# class CashierModel(Base):
+#     __tablename__ = "cashier"
+
+#     id = Column(Integer, primary_key=True, autoincrement=True)
